b79b9f6286947d8633c8922310a6a03a86a2083c
christophe gillis <chris.tim.gillis@gmail.com>
2019-05-20 14:22:15 +0200

    added validations to user and idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,5 @@
 class Idea < ApplicationRecord
   belongs_to :user
+
+  validate :title, :content, :moment_to_be_delivered
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :ideas
+
+  validates_presence_of :username, :email, :password
+  validate :date_must_be_in_future
+
+  private
+
+  def date_must_be_in_future
+    return if moment_to_be_delivered
+
+    errors.add(:moment_to_be_delivered, "can't be in past") if moment_to_be_delivered < DateTime.now
+  end
 end

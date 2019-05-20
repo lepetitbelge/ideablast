f29c19007cff3168caaf60e37229425334d44d26
christophe gillis <chris.tim.gillis@gmail.com>
2019-05-20 14:43:32 +0200

    updated validations

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,14 @@ class Idea < ApplicationRecord
   belongs_to :user
 
   validate :title, :content, :delivery_time
+
+  validate :date_must_be_in_future
+
+  private
+
+  def date_must_be_in_future
+    return if delivery_time
+
+    errors.add(:delivery_time, "can't be in past") if delivery_time < DateTime.now
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,5 @@ class User < ApplicationRecord
   has_many :ideas
 
   validates_presence_of :username, :email, :password
-  validate :date_must_be_in_future
-
-  private
-
-  def date_must_be_in_future
-    return if delivery_time
-
-    errors.add(:delivery_time, "can't be in past") if delivery_time < DateTime.now
-  end
+  validates :username, uniqueness: true
 end
